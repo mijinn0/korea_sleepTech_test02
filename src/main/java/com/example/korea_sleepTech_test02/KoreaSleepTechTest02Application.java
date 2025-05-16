df0bc1824863e29ec17c3732178bcac239c7e732
mijinn0 <pmj_0818@naver.com>
2025-05-16 09:28:01 +0900

    20250516 pmj test

---
+++ src/main/java/com/example/korea_sleepTech_test02/KoreaSleepTechTest02Application.java
@@ -0,0 +1,13 @@
+package com.example.korea_sleepTech_test02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaSleepTechTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaSleepTechTest02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/korea_sleepTech_test02/KoreaSleepTechTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.korea_sleepTech_test02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaSleepTechTest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
